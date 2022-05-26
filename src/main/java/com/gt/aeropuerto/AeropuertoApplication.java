b7ae7f63b28b94be870788549cd2ac86d6d859a1
JairoJS1 <jairo.j199912@gmail.com>
2022-05-26 02:16:47 -0600

    se envia forma de consultar

--- src/main/java/com/gt/aeropuerto/AeropuertoApplication.java
+++ src/main/java/com/gt/aeropuerto/AeropuertoApplication.java
@@ -17,7 +17,7 @@ public class AeropuertoApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://aeropuerto-backend.herokuapp.com").allowedMethods("*").allowedHeaders("*");
                                 
                                 
                                 
